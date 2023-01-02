bf1b8a5b7e9374274981686f78b1568445b7c0c4
DavidNikolov <72266534+DavidNikolov@users.noreply.github.com>
2023-01-02 15:06:34 +0200

    last push
    Car example finished

---
+++ StupidCar/StupidCar/Car.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupidCar
+{
+    public class Car
+    {
+
+        private string brand;
+        private string model;
+        private string color;
+        private int miles;
+        public string Brand {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public string Model {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public string Color {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public int Miles {
+            get { return miles; }
+            set { miles = value; }
+        }
+
+        public Car(string brand, string model, string color, int miles) {
+            this.Brand = brand;
+            this.Model = model;
+            this.Color = color;
+            this.Miles = miles;
+        }
+
+        public void Menu(List<Car> carList) { 
+            
+            while (true) {
+
+                Console.WriteLine("Your options are:");
+                Console.WriteLine("Add");
+                Console.WriteLine("Remove");
+                Console.WriteLine("ChangeColor");
+                Console.WriteLine("ChangeMiles");
+                Console.WriteLine("DisplayGarage");
+                Console.WriteLine("end");
+                Console.Write("Chose and option >> ");
+                string option = Console.ReadLine();
+                string brand, model, color;
+                int miles, index;
+
+                if (option == "End")
+                {
+                    break;
+                }
+
+                switch (option)
+                {
+                    case "Add":
+                        Console.Write("Enter your brand, model, color, miles");
+                        brand = Console.ReadLine();
+                        model = Console.ReadLine();
+                        color = Console.ReadLine();
+                        miles = int.Parse(Console.ReadLine());
+                        Car newCar = new Car(brand, model, color, miles);
+                        carList.Add(newCar);
+                        Console.Clear();
+                        break;
+
+                    case "Remove":
+                        Console.Write("Enter the number of the car you want to remove >>");
+                        index = int.Parse(Console.ReadLine());
+                        carList.RemoveAt(index - 1);
+                        Console.Clear();
+                        break;
+
+                    case "CahngeColor":
+                        Console.Write("Enter the number of the car you want to change color to >>");
+                        index = int.Parse(Console.ReadLine());
+                        Console.Write("what color do you want >>");
+                        color = Console.ReadLine();
+                        carList[index = 1].color = color;
+                        Console.Clear();
+                        break;
+
+                    case "CahngeMiles":
+                        Console.Write("Enter the number of the car you want to change the miles to >>");
+                        index = int.Parse(Console.ReadLine());
+                        Console.Write("enter the miles >>");
+                        miles = int.Parse(Console.ReadLine());
+                        carList[index = 1].miles = miles;
+                        Console.Clear();
+                        break;
+
+                    case "DisplayGarage":
+                        foreach (var item in carList) Console.WriteLine(item.ToString());
+                        Console.WriteLine("###################################");
+                        break;
+
+
+                }
+            }
+        }
+    }
+}
---
+++ StupidCar/StupidCar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+
+
+namespace StupidCar {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Enter your brand, model, color, miles ");
+            string brand = Console.ReadLine();
+            string model = Console.ReadLine();
+            string color = Console.ReadLine();
+            int miles = int.Parse(Console.ReadLine());
+            
+            List<Car> carList = new List<Car>();
+            Car car = new Car(brand, model, color, miles);
+            carList.Add(car);
+            car.Menu(carList);
+        }
+    }
+}
