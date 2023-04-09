143dfa321a522e5956c6760fcc18e9a1c50f315a
DavidNikolov <72266534+DavidNikolov@users.noreply.github.com>
2023-04-09 18:41:01 +0300

    commitment on many things
    YeetCode and Forcebite

---
+++ YeetCode/YeetCode/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace YeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string index = Console.ReadLine();
+                ReverseSimbols(index);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static void ReverseSimbols(string input)
+        {
+            char[] symbols = input.ToCharArray();
+
+            foreach (char i in symbols)
+            {
+                if (i == ' ') throw new ArgumentException("Invalid input");
+            }
+
+            for (int i = symbols.Length - 1; i >= 0; i--)
+            {
+                Console.Write(symbols[i]);
+            }
+        }
+    }
+}
